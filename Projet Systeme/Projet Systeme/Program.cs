9cbc480159938be301d32e0aabc21ddfb82bb327
Sophien <Sophien@PC-SOSO>
2018-12-07 16:12:18 +0100

    Test_contributor

--- Projet Systeme/Projet Systeme/Program.cs
+++ Projet Systeme/Projet Systeme/Program.cs
@@ -6,7 +6,7 @@ namespace Projet_Systeme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey Soso!");
         }
     }
 }
